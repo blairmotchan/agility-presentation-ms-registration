b4ec27f59a49e2d3b1c185655bdf5c1b6e378f0c
Blair Motchan <blair.motchan@gmail.com>
2018-08-24 22:01:21 -0500

    fixing deploy script

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -3,7 +3,7 @@
 ssh-keyscan  $DEPLOYMENT_SERVER >> ~/.ssh/known_hosts
 
 scp -i ~/.ssh/id_rsa_ab7c42aebeba55bc1d50f35b575e2325 \
-    agility-combined/target/agility-attendee-session.jar \
+    agility-combined/target/agility-attendee-registration.jar \
     $DEPLOYMENT_USER@$DEPLOYMENT_SERVER:/home/$DEPLOYMENT_USER
 
-ssh -i ~/.ssh/id_rsa_ab7c42aebeba55bc1d50f35b575e2325 $DEPLOYMENT_USER@$DEPLOYMENT_SERVER 'kill -9 $(lsof -t -i:8082) & /usr/bin/java -jar agility-attendee-session >> agility-attendee-session.log &'
+ssh -i ~/.ssh/id_rsa_ab7c42aebeba55bc1d50f35b575e2325 $DEPLOYMENT_USER@$DEPLOYMENT_SERVER 'kill -9 $(lsof -t -i:8083) & /usr/bin/java -jar agility-attendee-registration.jar >> agility-attendee-registration.log &'
